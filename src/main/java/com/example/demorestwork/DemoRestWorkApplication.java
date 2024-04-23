d319008a91fdaf826d70337ecb767bd610586df2
E_Kazak <kazak-zhenya2017@mail.ru>
2024-04-23 22:43:05 +0300

    init project

---
+++ src/main/java/com/example/demorestwork/DemoRestWorkApplication.java
@@ -0,0 +1,13 @@
+package com.example.demorestwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRestWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoRestWorkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demorestwork/DemoRestWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demorestwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRestWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
